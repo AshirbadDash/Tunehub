d01ac94f1239cb90376851c3a460fd1436d64b42
ashirbad dash <ashirbadash2000@gmail.com>
2024-02-16 17:33:02 +0530

    "tunehub01"

---
+++ src/main/java/com/kodnest/projecttunehub/entity/User.java
@@ -0,0 +1,100 @@
+package com.kodnest.projecttunehub.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    String userid;
+    String username;
+    String email;
+    String password;
+    String gender;
+    String role;
+    String address;
+
+
+    public User() {
+        super();
+    }
+
+    public User(String userid, String username, String email, String password, String gender, String role, String address) {
+        this.userid = userid;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.role = role;
+        this.address = address;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userid='" + userid + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", gender='" + gender + '\'' +
+                ", role='" + role + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
