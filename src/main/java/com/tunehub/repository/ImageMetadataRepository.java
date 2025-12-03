c8a2e22d4996ce2ac21aecbc6b63b635b6da5bde
Ashirbad Dash <ashirbadash2000@gmail.com>
2025-12-04 01:43:54 +0530

    Added favicon

--- src/main/java/com/tunehub/repository/ImageMetadataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tunehub.repository;
-
-import com.tunehub.model.entity.ImageMetadata;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageMetadataRepository extends JpaRepository<ImageMetadata, String> {
-
-}
